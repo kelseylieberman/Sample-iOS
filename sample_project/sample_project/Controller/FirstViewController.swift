80fa8c901a6e5502bbd67481ce7f40cde6edd89c
kelseylieberman <36453545+kelseylieberman@users.noreply.github.com>
2020-08-27 20:14:58 -0700

    Basic Outline

---
+++ sample_project/sample_project/Controller/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  sample_project
+//
+//  Created by Kelsey Lieberman on 8/27/20.
+//  Copyright © 2020 Kelsey Lieberman. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ sample_project/sample_projectTests/sample_projectTests.swift
@@ -0,0 +1,34 @@
+//
+//  sample_projectTests.swift
+//  sample_projectTests
+//
+//  Created by Kelsey Lieberman on 8/27/20.
+//  Copyright © 2020 Kelsey Lieberman. All rights reserved.
+//
+
+import XCTest
+@testable import sample_project
+
+class sample_projectTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
